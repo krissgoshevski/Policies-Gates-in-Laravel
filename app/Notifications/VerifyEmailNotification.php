aac4d82b96efd91dca01070ec52f6b4527140152
Kristijan <97643470+krissgoshevski@users.noreply.github.com>
2024-05-26 13:14:06 +0200

    Update VerifyEmailNotification.php

--- app/Notifications/VerifyEmailNotification.php
+++ app/Notifications/VerifyEmailNotification.php
@@ -49,7 +49,7 @@ class VerifyEmailNotification extends Notification implements ShouldQueue
         ->line('Thank you for registering. Please click the button below to verify your email address.')
         ->action('Verify Email Address', $verificationUrl)
         ->line('If you did not create an account, no further action is required.')
-        ->salutation('Regards, Your Neotel Team');
+        ->salutation('Regards, Your Team');
     }
 
 
